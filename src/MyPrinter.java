4f35563ffb2a3f6231baedca97f3845953a09727
melmanss <74724829+melmanss@users.noreply.github.com>
2025-02-07 20:23:26 +0200

    MyPrinter.java

--- src/MyPrinter.java
+++ src/MyPrinter.java
@@ -16,24 +16,19 @@ public class MyPrinter implements Printer {
     public static class Message {
         private String text;
         private String sender;
-
         public Message(String text, String sender) {
             this.text = text;
             this.sender = sender;
         }
-
         public String getText() {
             return text;
         }
-
         public String getSender() {
             return sender;
         }
-
         public void setText(String text) {
             this.text = text;
         }
-
         public void setSender(String sender) {
             this.sender = sender;
         }
