2d8b8ef57e4a2fb0e8cd4bf74ce5becc3c0e99fa
melmanss <74724829+melmanss@users.noreply.github.com>
2025-02-08 14:36:29 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Printer printer = new MyPrinter();
-
         while (true) {
             System.out.print("Введіть текст повідомлення (або 'exit' для виходу): ");
             String text = scanner.nextLine();
             if (text.equalsIgnoreCase("exit")) {
                 break; // Вихід з циклу, якщо введено "exit"
             }
-
             System.out.print("Введіть ім'я відправника (можна залишити порожнім): ");
             String sender = scanner.nextLine();
-
             // Виклик методу print
             printer.print(sender, text);
         }
-
         scanner.close(); // Закриття сканера
     }
 }
